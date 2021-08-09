a798b6d49faa11ee6ae221ad621ba20560c71e82
hyunmin0317 <choihm9903@naver.com>
2021-08-09 14:50:36 +0900

    Update Part1

---
+++ Part1/src/ch14/IfElseTest.java
@@ -0,0 +1,20 @@
+package ch14;
+
+public class IfElseTest {
+
+	public static void main(String[] args) {
+		int age = 7;
+
+		//Scanner scanner = new Scanner(System.in);
+		//int age = scanner.nextInt();
+		
+		if( age >= 8) {
+			System.out.println("학교에 다닙니다");
+		}
+		else {
+			System.out.println("학교에 다니지 않습니다.");
+		}
+
+	}
+
+}
