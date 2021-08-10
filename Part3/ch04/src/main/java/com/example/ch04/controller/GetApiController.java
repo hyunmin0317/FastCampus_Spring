984687bf68830dcdebda2a8196ea19786a137b50
hyunmin0317 <choihm9903@naver.com>
2021-08-10 12:29:19 +0900

    Update Part3

--- Part3/ch04/src/main/java/com/example/ch04/controller/GetApiController.java
+++ Part3/ch04/src/main/java/com/example/ch04/controller/GetApiController.java
@@ -1,6 +1,9 @@
 package com.example.ch04.controller;
 
+import com.example.ch04.dto.UserRequest;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/get")
@@ -29,11 +32,45 @@ public class GetApiController {
         return name;
     }
 
+//     Query
+//     ?key=value&key2=value2
 //     http://localhost:8080/api/get/query-param?user=steve&email=steve@gmail.com&age=30
 
+    @GetMapping(path = "query-param")
+    public String queryParam(@RequestParam Map<String, String> queryParam){
 
-    // Query
-    // ?key=value&key2=value2
+        StringBuilder sb = new StringBuilder();
+        queryParam.entrySet().forEach( entry -> {
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+            System.out.println("\n");
 
+            sb.append(entry.getKey()+" = "+entry.getValue()+"\n");
+        });
 
+        return sb.toString();
+    }
+
+    @GetMapping("query-param02")
+    public String queryParam02(
+            @RequestParam String name,
+            @RequestParam String email,
+            @RequestParam int age
+            ){
+
+        System.out.println(name);
+        System.out.println(email);
+        System.out.println(age);
+
+        return name+ " " + email + " " + age;
+    }
+
+    @GetMapping("query-param03")
+    public String queryParam03(UserRequest userRequest){
+        System.out.println(userRequest.getName());
+        System.out.println(userRequest.getEmail());
+        System.out.println(userRequest.getAge());
+
+        return userRequest.toString();
+    }
 }
