1510f8a574151b2b2f804746c01642f5c7a1e80b
hyunmin0317 <choihm9903@naver.com>
2021-08-10 15:07:29 +0900

    Update Part3

---
+++ Part3/ch04/post/src/main/java/com/example/post/controller/PostApiController.java
@@ -0,0 +1,20 @@
+package com.example.post.controller;
+
+import com.example.post.dto.PostRequestDto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class PostApiController {
+
+    @PostMapping("/post")
+    public void post(@RequestBody PostRequestDto requestData){
+        System.out.println(requestData);
+
+    }
+}
