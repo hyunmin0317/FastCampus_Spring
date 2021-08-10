baffbe3c09f9320eec0ddc7804501fc83b2b12a7
hyunmin0317 <choihm9903@naver.com>
2021-08-10 15:48:18 +0900

    Update Part3

---
+++ Part3/ch04/delete/src/main/java/com/example/delete/controller/DeleteApiController.java
@@ -0,0 +1,16 @@
+package com.example.delete.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class DeleteApiController {
+
+    @DeleteMapping("/delete/{userId}")
+    public void delete(@PathVariable String userId, @RequestParam String account){
+        System.out.println(userId);
+        System.out.println(account);
+
+        // delete -> 리소스 삭제 200 ok
+    }
+}
