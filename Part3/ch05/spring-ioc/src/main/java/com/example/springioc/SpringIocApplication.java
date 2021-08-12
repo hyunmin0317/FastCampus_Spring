33dcfd97dddd158471262a0fb121c6b4b6f51b31
hyunmin0317 <choihm9903@naver.com>
2021-08-12 10:58:55 +0900

    Update Part3

---
+++ Part3/ch05/spring-ioc/src/main/java/com/example/springioc/SpringIocApplication.java
@@ -0,0 +1,13 @@
+package com.example.springioc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIocApplication.class, args);
+	}
+
+}
