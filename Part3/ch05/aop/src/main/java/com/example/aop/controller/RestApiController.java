5f316c341dcb92cafe86618d6c743177a4ffec81
hyunmin0317 <choihm9903@naver.com>
2021-08-13 13:33:00 +0900

    Update Part3

--- Part3/ch05/aop/src/main/java/com/example/aop/controller/RestApiController.java
+++ Part3/ch05/aop/src/main/java/com/example/aop/controller/RestApiController.java
@@ -1,6 +1,9 @@
 package com.example.aop.controller;
 
+import com.example.aop.annotation.Decode;
+import com.example.aop.annotation.Timer;
 import com.example.aop.dto.User;
+import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,6 +17,22 @@ public class RestApiController {
 
     @PostMapping("/post")
     public User post(@RequestBody User user){
+        return user;
+    }
+
+    @Timer
+    @DeleteMapping("/delete")
+    public void delete() throws InterruptedException {
+        // db logic
+
+        Thread.sleep(1000 * 2);
+    }
+
+    @Decode
+    @PutMapping("/put")
+    public User put(@RequestBody User user){
+        System.out.println("put");
+        System.out.println(user);
         return user;
     }
 }
