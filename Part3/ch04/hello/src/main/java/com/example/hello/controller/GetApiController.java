bf7e03e44a4d841cbdede131eb9e3237ddfb8ce1
hyunmin0317 <choihm9903@naver.com>
2021-08-20 20:43:10 +0900

    Spring-GET

--- Part3/ch04/hello/src/main/java/com/example/hello/controller/GetApiController.java
+++ Part3/ch04/hello/src/main/java/com/example/hello/controller/GetApiController.java
@@ -1,6 +1,9 @@
 package com.example.hello.controller;
 
+import com.example.hello.dto.UserRequest;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/get")
@@ -29,5 +32,44 @@ public class GetApiController {
     public String pathVariable(@PathVariable(name = "name") String pathName, String name) {
         System.out.println("PathVariable : "+pathName);
         return pathName;
+    }
+
+    // Query Parameter
+
+    // 1. Key가 정해지지 않은 경우
+    // http://localhost:9090/api/get/query-param?user=steve&email=steve@gmail.com&age=30
+    @GetMapping("query-param")
+    public String queryParam(@RequestParam Map<String, String> queryParam) {
+
+        StringBuilder sb = new StringBuilder();
+        queryParam.entrySet().forEach( entry -> {
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+            System.out.println("\n");
+
+            sb.append(entry.getKey()+" = "+entry.getValue()+"\n");
+        });
+
+        return sb.toString();
+    }
+
+    // 2. Key가 정해진 경우
+    @GetMapping("query-param02")
+    public String queryParam02(
+            @RequestParam String name,
+            @RequestParam String email,
+            @RequestParam int age
+    ){
+        return name+" "+email+" "+age;
+    }
+
+    // 3. dto 방법
+    @GetMapping("query-param03")
+    public String queryParam03(UserRequest userRequest){
+        System.out.println(userRequest.getName());
+        System.out.println(userRequest.getEmail());
+        System.out.println(userRequest.getAge());
+
+        return userRequest.toString();
     }
 }
