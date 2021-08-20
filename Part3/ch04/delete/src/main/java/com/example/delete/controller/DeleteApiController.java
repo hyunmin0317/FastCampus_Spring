b1473a6051e239ced9dca6c9216c8bc3ff846202
hyunmin0317 <choihm9903@naver.com>
2021-08-20 21:30:36 +0900

    Spring-DELETE

--- Part3/ch04/delete/src/main/java/com/example/delete/controller/DeleteApiController.java
+++ Part3/ch04/delete/src/main/java/com/example/delete/controller/DeleteApiController.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.*;
 public class DeleteApiController {
 
     @DeleteMapping("/delete/{userId}")
-    public void delete(@PathVariable String userId, @RequestParam String account){
+    public void delete(@PathVariable String userId, @RequestParam String account) {
         System.out.println(userId);
         System.out.println(account);
 
-        // delete -> 리소스 삭제 200 ok
+        // delete -> 리소스 삭제 200 OK
     }
 }
