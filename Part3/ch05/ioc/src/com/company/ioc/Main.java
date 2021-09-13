634dd2dcbf9d6f129ee557bda4a3ebe6fb77f60e
hyunmin0317 <choihm9903@naver.com>
2021-09-13 19:43:22 +0900

    Update ioc

---
+++ Part3/ch05/ioc/src/com/company/ioc/Main.java
@@ -0,0 +1,18 @@
+package com.company.ioc;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String url = "www.naver.com/books/it?page=10&size=20&name=spring-boot";
+
+        // Base 64 encoding
+        Encoder encoder = new Encoder(new Base64Encoder());
+        String result = encoder.encode(url);
+        System.out.println(result);
+
+        // url encoding
+        encoder = new Encoder(new UrlEncoder());
+        result = encoder.encode(url);
+        System.out.println(result);
+    }
+}
