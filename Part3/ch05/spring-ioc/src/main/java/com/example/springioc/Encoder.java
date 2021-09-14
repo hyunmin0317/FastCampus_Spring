f7cb73f7202a7106c73fbd6f97e87f04c4755a7b
hyunmin0317 <choihm9903@naver.com>
2021-09-14 14:22:53 +0900

    Update spring-ioc

---
+++ Part3/ch05/spring-ioc/src/main/java/com/example/springioc/Encoder.java
@@ -0,0 +1,14 @@
+package com.example.springioc;
+
+public class Encoder {
+
+    private IEncoder iEncoder;
+
+    public Encoder(IEncoder iEncoder) {
+        this.iEncoder = iEncoder;
+    }
+
+    public String encode(String message) {
+        return iEncoder.encode(message);
+    }
+}
