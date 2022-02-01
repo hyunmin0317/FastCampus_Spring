8b1e208b184d417abe37eb02d547caf2aba4ebe6
hyunmin0317 <choihm9903@naver.com>
2022-02-01 16:08:19 +0900

    feat: Server2Server Naver-API

--- Part3/ch07/server/src/main/java/com/example/server/controller/ServerApiController.java
+++ Part3/ch07/server/src/main/java/com/example/server/controller/ServerApiController.java
@@ -3,12 +3,54 @@ package com.example.server.controller;
 import com.example.server.dto.Req;
 import com.example.server.dto.User;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/server")
 public class ServerApiController {
+
+    // https://openapi.naver.com/v1/search/local.json
+    // ?query=station
+    // &display=10
+    // &start=1
+    // &sort=random
+    @GetMapping("/naver")
+    public String naver() {
+
+        URI uri = UriComponentsBuilder
+                .fromUriString("https://openapi.naver.com")
+                .path("/v1/search/local.json")
+                .queryParam("query", "갈비집")
+                .queryParam("display", 10)
+                .queryParam("start", 1)
+                .queryParam("sort", "random")
+                .encode(Charset.forName("UTF-8"))
+                .build()
+                .toUri();
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        RequestEntity<Void> req = RequestEntity
+                .get(uri)
+                .header("X-Naver-Client-Id", "elgL1mV7finz6b0DVleQ")
+                .header("X-Naver-Client-Secret", "H1SQXc3Cn8")
+                .build();
+
+        ResponseEntity<String> result = restTemplate.exchange(req, String.class);
+
+        return result.getBody();
+    }
+
 
     @GetMapping("/hello")
     public User hello(@RequestParam String name, @RequestParam int age) {
