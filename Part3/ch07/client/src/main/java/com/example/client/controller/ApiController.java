d7e88abcfbc0f019265e754cac17bd8900d35dcd
hyunmin0317 <choihm9903@naver.com>
2022-02-01 14:59:30 +0900

    feat: Server2Server POST

--- Part3/ch07/client/src/main/java/com/example/client/controller/ApiController.java
+++ Part3/ch07/client/src/main/java/com/example/client/controller/ApiController.java
@@ -1,5 +1,6 @@
 package com.example.client.controller;
 
+import com.example.client.dto.UserResponse;
 import com.example.client.service.RestTemplateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,6 +23,11 @@ public class ApiController {
     @GetMapping("/hello")
     public String getHello() {
         return restTemplateService.hello();
+    }
+
+    @GetMapping("hello2")
+    public UserResponse getHello2() {
+        return restTemplateService.post();
     }
 
 }
--- Part3/ch07/client/src/main/java/com/example/client/service/RestTemplateService.java
+++ Part3/ch07/client/src/main/java/com/example/client/service/RestTemplateService.java
@@ -1,5 +1,6 @@
 package com.example.client.service;
 
+import com.example.client.dto.UserRequest;
 import com.example.client.dto.UserResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -35,4 +36,30 @@ public class RestTemplateService {
         return result.getBody().toString();
     }
 
+    // http://localhost:9090/api/server/user/{userId}/name/{userName}
+    public UserResponse post() {
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("/api/server/user/{userId}/name/{userName}")
+                .encode()
+                .build()
+                .expand(100, "steve")
+                .toUri();
+
+        System.out.println(uri);
+
+        // http body -> object -> object mapper -> json -> rest template -> http body json
+        UserRequest req = new UserRequest();
+        req.setName("steve");
+        req.setAge(10);
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<UserResponse> response = restTemplate.postForEntity(uri, req, UserResponse.class);
+
+        System.out.println(response.getStatusCode());
+        System.out.println(response.getHeaders());
+        System.out.println(response.getBody());
+
+        return response.getBody();
+    }
 }
