57e79a6849ffe9698010f88a963ddf19f45a5c6b
hyunmin <choihm9903@naver.com>
2022-03-10 11:34:02 +0900

    Part4-1: Query Method

--- Part4-1 Spring-JPA/src/main/java/com/fastcampus/jpa/bookmanager/repository/UserRepository.java
+++ Part4-1 Spring-JPA/src/main/java/com/fastcampus/jpa/bookmanager/repository/UserRepository.java
@@ -3,5 +3,31 @@ package com.fastcampus.jpa.bookmanager.repository;
 import com.fastcampus.jpa.bookmanager.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findByName(String name);
+    User findByEmail(String email);
+    User getByEmail(String email);
+    User readByEmail(String email);
+    User queryByEmail(String email);
+    User searchByEmail(String email);
+    User streamByEmail(String email);
+    User findUserByEmail(String email);
+    User findSomethingByEmail(String email);
+
+    List<User> findFirst2ByName(String name);
+    List<User> findTop2ByName(String name);
+    List<User> findLast1ByName(String name);
+    List<User> findByEmailAndName(String email, String name);
+    List<User> findByEmailOrName(String email, String name);
+    List<User> findByCreatedAtAfter(LocalDateTime yesterday);
+    List<User> findByIdAfter(Long id);
+    List<User> findByCreatedAtGreaterThan(LocalDateTime yesterday);
+    List<User> findByCreatedAtGreaterThanEqual(LocalDateTime yesterday);
+    List<User> findByCreatedAtBetween(LocalDateTime yesterday, LocalDateTime tomorrow);
+    List<User> findByIdBetween(Long id1, Long id2);
+    List<User> findByIdGreaterThanEqualAndIdLessThanEqual(Long id1, Long id2);
+    List<User> findByIdIsNotNull();
 }
--- Part4-1 Spring-JPA/src/test/java/com/fastcampus/jpa/bookmanager/repository/UserRepositoryTest.java
+++ Part4-1 Spring-JPA/src/test/java/com/fastcampus/jpa/bookmanager/repository/UserRepositoryTest.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.*;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.springframework.data.domain.ExampleMatcher.GenericPropertyMatchers.contains;
@@ -130,5 +132,32 @@ class UserRepositoryTest {
         user.setEmail("matin-update@fastcampus.com");
 
         userRepository.save(user);
+    }
+
+    @Test
+    void select() {
+        System.out.println(userRepository.findByName("dennis"));
+        System.out.println("findByEmail : " + userRepository.findByEmail("martin@fastcampus.com"));
+        System.out.println("getByEmail : " + userRepository.getByEmail("martin@fastcampus.com"));
+        System.out.println("readByEmail : " + userRepository.readByEmail("martin@fastcampus.com"));
+        System.out.println("queryByEmail : " + userRepository.queryByEmail("martin@fastcampus.com"));
+        System.out.println("searchByEmail : " + userRepository.searchByEmail("martin@fastcampus.com"));
+        System.out.println("streamByEmail : " + userRepository.streamByEmail("martin@fastcampus.com"));
+        System.out.println("findUserByEmail : " + userRepository.findUserByEmail("martin@fastcampus.com"));
+        System.out.println("findSomethingByEmail : " + userRepository.findSomethingByEmail("martin@fastcampus.com"));
+
+        System.out.println("findTop2ByName : " + userRepository.findTop2ByName("martin"));
+        System.out.println("findFirst2ByName : " + userRepository.findFirst2ByName("martin"));
+        System.out.println("findLast1ByName : " + userRepository.findLast1ByName("martin"));
+        System.out.println("findByEmailAndName : " + userRepository.findByEmailAndName("martin@fastcampus.com", "martin"));
+        System.out.println("findByEmailOrName : " + userRepository.findByEmailOrName("martin@fastcampus.com", "dennis"));
+        System.out.println("findByCreatedAtAfter : " + userRepository.findByCreatedAtAfter(LocalDateTime.now().minusDays(1L)));
+        System.out.println("findByIdAfter : " + userRepository.findByIdAfter(4L));
+        System.out.println("findByCreatedAtGreaterThan : " + userRepository.findByCreatedAtGreaterThan(LocalDateTime.now().minusDays(1L)));
+        System.out.println("findByCreatedAtGreaterThanEqual : " + userRepository.findByCreatedAtGreaterThanEqual(LocalDateTime.now().minusDays(1L)));
+        System.out.println("findByCreatedAtBetween : " + userRepository.findByCreatedAtBetween(LocalDateTime.now().minusDays(1L), LocalDateTime.now().plusDays(1L)));
+        System.out.println("findByIdBetween : " + userRepository.findByIdBetween(1L, 3L));
+        System.out.println("findByIdGreaterThanEqualAndIdLessThanEqual : " + userRepository.findByIdGreaterThanEqualAndIdLessThanEqual(1L, 3L));
+        System.out.println("findByIdIsNotNull : " + userRepository.findByIdIsNotNull());
     }
 }
